146a5025a332922c2e6cb963696e841b9fadaee1
MadRat96 <mischa.alpeew@yandex.ru>
2021-10-11 14:14:56 +0300

    Обновил вёрстку и код

--- library/src/main/java/yuku/ambilwarna/FGHColorPickerDialog.kt
+++ library/src/main/java/yuku/ambilwarna/FGHColorPickerDialog.kt
@@ -19,40 +19,13 @@ class FGHColorPickerDialog(
 ) {
     interface OnnDialogButtonClickedListener {
         fun onCancel(dialog: FGHColorPickerDialog?)
+        
         fun onOk(dialog: FGHColorPickerDialog?, color: Int)
     }
     
     private val dialog: AlertDialog
     private val currentColorHSV = FloatArray(3)
     private val binding: AmbilwarnaDialogBinding
-    
-    protected fun moveCursor() {
-        with(binding) {
-            var y = viewHue.measuredHeight - colorHue * viewHue.measuredHeight / 360f
-            if (y == viewHue.measuredHeight.toFloat()) {
-                y = 0f
-            }
-            val layoutParams = viewCursor.layoutParams as RelativeLayout.LayoutParams
-            layoutParams.leftMargin = (viewHue.left - floor((viewCursor.measuredWidth / 2).toDouble())
-                    - viewContainer.paddingLeft).toInt()
-            layoutParams.topMargin = (viewHue.top + y - floor((viewCursor.measuredHeight / 2).toDouble())
-                - viewContainer.paddingTop).toInt()
-            viewCursor.layoutParams = layoutParams
-        }
-    }
-    
-    protected fun moveTarget() {
-        with(binding) {
-            val x = colorSaturation * viewSatVal.measuredWidth
-            val y = (1f - colorValue) * viewSatVal.measuredHeight
-            val layoutParams = viewTarget.layoutParams as RelativeLayout.LayoutParams
-            layoutParams.leftMargin = (viewSatVal.left + x - floor((viewTarget.measuredWidth / 2).toDouble())
-                - viewContainer.paddingLeft).toInt()
-            layoutParams.topMargin = (viewSatVal.top + y - floor((viewTarget.measuredHeight / 2).toDouble())
-                - viewContainer.paddingTop).toInt()
-            viewTarget.layoutParams = layoutParams
-        }
-    }
     
     private val color: Int
         get() {
@@ -75,79 +48,32 @@ class FGHColorPickerDialog(
             currentColorHSV[2] = value
         }
     private var alpha: Int
-    private fun receiveAlpha(): Float {
-        return alpha.toFloat()
-    }
-    private fun updateAlpha(alpha: Int) {
-        this.alpha = alpha
-    }
-    
-    fun show() {
-        dialog.show()
-    }
     
     init {
-        Color.colorToHSV(currentColor, currentColorHSV)
+        Color.colorToHSV(
+            currentColor,
+            currentColorHSV
+        )
         alpha = Color.alpha(currentColor)
         binding = AmbilwarnaDialogBinding.inflate(
             LayoutInflater.from(context)
         )
         with(binding) {
-            viewSatVal.setHue(colorHue)
+            saturationAndValueSelectionView.setHue(colorHue)
             viewOldColor.setBackgroundColor(currentColor)
             viewNewColor.setBackgroundColor(currentColor)
-            viewHue.setOnTouchListener { view: View?, motionEvent: MotionEvent ->
-                if (motionEvent.action == MotionEvent.ACTION_MOVE ||
-                    motionEvent.action == MotionEvent.ACTION_DOWN ||
-                    motionEvent.action == MotionEvent.ACTION_UP
-                ) {
-                    var y = motionEvent.y
-                    if (y < 0f) {
-                        y = 0f
-                    }
-                    if (y > viewHue.measuredHeight) {
-                        y = viewHue.measuredHeight - 0.001f // to avoid jumping the cursor from bottom to top.
-                    }
-                    var hue = 360f - 360f / viewHue.measuredHeight * y
-                    if (hue == 360f) {
-                        hue = 0f
-                    }
-                    colorHue = hue
-                    // update view
-                    viewSatVal.setHue(hue)
-                    moveCursor()
-                    viewNewColor.setBackgroundColor(color)
-                    return@setOnTouchListener true
-                }
-                false
+            saturationAndValueSelectionView.setOnTouchListener { view: View?, motionEvent: MotionEvent ->
+                onSaturationAndValueSelectionViewTouched(
+                    view,
+                    motionEvent
+                )
             }
-            viewSatVal.setOnTouchListener { view: View?, motionEvent: MotionEvent ->
-                if (motionEvent.action == MotionEvent.ACTION_MOVE || motionEvent.action == MotionEvent.ACTION_DOWN || motionEvent.action == MotionEvent.ACTION_UP) {
-                    // touch event are in dp units.
-                    var x = motionEvent.x
-                    var y = motionEvent.y
-                    if (x < 0f) {
-                        x = 0f
-                    }
-                    if (x > viewSatVal.measuredWidth) {
-                        x = viewSatVal.measuredWidth.toFloat()
-                    }
-                    if (y < 0f) {
-                        y = 0f
-                    }
-                    if (y > viewSatVal.measuredHeight){
-                        y = viewSatVal.measuredHeight.toFloat()
-                    }
-                    colorSaturation = 1f / viewSatVal.measuredWidth * x
-                    colorValue = 1f - 1f / viewSatVal.measuredHeight * y
-                    // update view
-                    moveTarget()
-                    viewNewColor.setBackgroundColor(color)
-                    return@setOnTouchListener true
-                }
-                false
+            hueSelectionView.setOnTouchListener { view: View?, motionEvent: MotionEvent ->
+                onHueSelectionViewTouched(
+                    view,
+                    motionEvent
+                )
             }
-    
             // if back button is used, call back our listener.
             dialog = AlertDialog.Builder(context).apply {
                 setPositiveButton(android.R.string.ok) { _: DialogInterface?, _: Int ->
@@ -170,18 +96,120 @@ class FGHColorPickerDialog(
                 }
             }.create()
             // kill all padding from the dialog window
-            dialog.setView(binding.root, 0, 0, 0, 0)
-    
+            dialog.setView(
+                binding.root,
+                0,
+                0,
+                0,
+                0
+            )
             // move cursor & target on first draw
             binding.root.viewTreeObserver.apply {
                 addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
                     override fun onGlobalLayout() {
-                        moveCursor()
-                        moveTarget()
-                        binding.root.viewTreeObserver.removeGlobalOnLayoutListener(this)
+                        moveHueSelector()
+                        moveSaturationAndValueSelector()
+                        binding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
                     }
                 })
             }
         }
+    }
+    
+    private fun onSaturationAndValueSelectionViewTouched(
+        view: View?,
+        motionEvent: MotionEvent
+    ): Boolean {
+        with(binding) {
+            if (motionEvent.action == MotionEvent.ACTION_MOVE ||
+                motionEvent.action == MotionEvent.ACTION_DOWN ||
+                motionEvent.action == MotionEvent.ACTION_UP) {
+                // touch event are in dp units.
+                var x = motionEvent.x
+                var y = motionEvent.y
+                if (x < 0f) {
+                    x = 0f
+                }
+                if (x > saturationAndValueSelectionView.measuredWidth) {
+                    x = saturationAndValueSelectionView.measuredWidth.toFloat()
+                }
+                if (y < 0f) {
+                    y = 0f
+                }
+                if (y > saturationAndValueSelectionView.measuredHeight) {
+                    y = saturationAndValueSelectionView.measuredHeight.toFloat()
+                }
+                colorSaturation = 1f / saturationAndValueSelectionView.measuredWidth * x
+                colorValue = 1f - 1f / saturationAndValueSelectionView.measuredHeight * y
+                // update view
+                moveSaturationAndValueSelector()
+                viewNewColor.setBackgroundColor(color)
+                return true
+            }
+            return false
+        }
+    }
+    
+    private fun moveSaturationAndValueSelector() {
+        with(binding) {
+            val x = colorSaturation * saturationAndValueSelectionView.measuredWidth
+            val y = (1f - colorValue) * saturationAndValueSelectionView.measuredHeight
+            val layoutParams = saturationAndValueSelectorView.layoutParams as RelativeLayout.LayoutParams
+            layoutParams.leftMargin = (saturationAndValueSelectionView.left + x - floor((saturationAndValueSelectorView.measuredWidth / 2).toDouble())
+                - viewContainer.paddingLeft).toInt()
+            layoutParams.topMargin = (saturationAndValueSelectionView.top + y - floor((saturationAndValueSelectorView.measuredHeight / 2).toDouble())
+                - viewContainer.paddingTop).toInt()
+            saturationAndValueSelectorView.layoutParams = layoutParams
+        }
+    }
+    
+    private fun onHueSelectionViewTouched(
+        view: View?,
+        motionEvent: MotionEvent
+    ): Boolean {
+        with(binding) {
+            if (motionEvent.action == MotionEvent.ACTION_MOVE ||
+                motionEvent.action == MotionEvent.ACTION_DOWN ||
+                motionEvent.action == MotionEvent.ACTION_UP
+            ) {
+                var y = motionEvent.y
+                if (y < 0f) {
+                    y = 0f
+                }
+                if (y > hueSelectionView.measuredHeight) {
+                    y = hueSelectionView.measuredHeight - 0.001f // to avoid jumping the cursor from bottom to top.
+                }
+                var hue = 360f - 360f / hueSelectionView.measuredHeight * y
+                if (hue == 360f) {
+                    hue = 0f
+                }
+                colorHue = hue
+                // update view
+                saturationAndValueSelectionView.setHue(hue)
+                moveHueSelector()
+                viewNewColor.setBackgroundColor(color)
+                return true
+            }
+            return false
+        }
+    }
+    
+    private fun moveHueSelector() {
+        with(binding) {
+            var y = hueSelectionView.measuredHeight - colorHue * hueSelectionView.measuredHeight / 360f
+            if (y == hueSelectionView.measuredHeight.toFloat()) {
+                y = 0f
+            }
+            val layoutParams = hueSelectorView.layoutParams as RelativeLayout.LayoutParams
+            layoutParams.leftMargin = (hueSelectionView.left - floor((hueSelectorView.measuredWidth / 2).toDouble())
+                    - viewContainer.paddingLeft).toInt()
+            layoutParams.topMargin = (hueSelectionView.top + y - floor((hueSelectorView.measuredHeight / 2).toDouble())
+                - viewContainer.paddingTop).toInt()
+            hueSelectorView.layoutParams = layoutParams
+        }
+    }
+    
+    fun show() {
+        dialog.show()
     }
 }
