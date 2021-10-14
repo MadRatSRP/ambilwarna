4aed43b0eababed70fd547998ab7aa3463c2fb1f
MadRat96 <mischa.alpeew@yandex.ru>
2021-10-14 16:40:42 +0300

    Отрефакторил MainActivity

--- demo/src/main/java/yuku/ambilwarna/demo/MainActivity.kt
+++ demo/src/main/java/yuku/ambilwarna/demo/MainActivity.kt
@@ -1,56 +1,58 @@
-package yuku.ambilwarna.demo;
+package yuku.ambilwarna.demo
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-import yuku.ambilwarna.FGHColorPickerDialog;
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import yuku.ambilwarna.FGHColorPickerDialog
+import yuku.ambilwarna.FGHColorPickerDialog.OnnDialogButtonClickedListener
+import yuku.ambilwarna.demo.databinding.ActivityMainBinding
 
-public class AmbilWarnaDemoActivity extends Activity {
-	TextView text1;
-	int color = 0xffffff00;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		final View button1 = findViewById(R.id.button1);
-		final View button2 = findViewById(R.id.button2);
-		final View button3 = findViewById(R.id.button3);
-		text1 = (TextView) findViewById(R.id.text1);
-		displayColor();
-
-		button1.setOnClickListener(view -> openDialog(false));
-
-		button2.setOnClickListener(view -> openDialog(true));
-
-		button3.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), AmbilWarnaDemoPreferenceActivity.class)));
-	}
-
-	void openDialog(boolean supportsAlpha) {
-		FGHColorPickerDialog dialog = new FGHColorPickerDialog(
-			AmbilWarnaDemoActivity.this,
-			color,
-			new FGHColorPickerDialog.OnnDialogButtonClickedListener() {
-			@Override
-			public void onOk(FGHColorPickerDialog dialog, int color) {
-				Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
-				AmbilWarnaDemoActivity.this.color = color;
-				displayColor();
-			}
-
-			@Override
-			public void onCancel(FGHColorPickerDialog dialog) {
-				Toast.makeText(getApplicationContext(), "cancel", Toast.LENGTH_SHORT).show();
-			}
-		});
-		dialog.show();
-	}
-
-	void displayColor() {
-		text1.setText(String.format("Current color: 0x%08x", color));
-	}
+class MainActivity : Activity() {
+    private lateinit var binding: ActivityMainBinding
+    private var currentColor: Long = 0xffff0000
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+        displayColor()
+        
+        binding.showDialogButton.setOnClickListener {
+            openDialog()
+        }
+    }
+    
+    private fun openDialog() {
+        val dialog = FGHColorPickerDialog(
+            this,
+            currentColor.toInt(),
+            object : OnnDialogButtonClickedListener {
+                override fun onOk(
+                    dialog: FGHColorPickerDialog?,
+                    color: Int
+                ) {
+                    Toast.makeText(applicationContext, "ok", Toast.LENGTH_SHORT).show()
+                    this@MainActivity.currentColor = color.toLong()
+                    displayColor()
+                }
+                
+                override fun onCancel(dialog: FGHColorPickerDialog?) {
+                    Toast.makeText(
+                        applicationContext,
+                        "cancel",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        )
+        dialog.show()
+    }
+    
+    fun displayColor() {
+        binding.currentColorHex.text = String.format(
+            "Current color: 0x%08x",
+            currentColor
+        )
+    }
 }
